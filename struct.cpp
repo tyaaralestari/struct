08a0f1c81e3b93d708a28a76ff25a79a333dca43
tyaaralestari <c030324111@mahasiswa.poliban.ac.id>
2025-06-01 11:18:52 +0800

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -12,23 +12,20 @@ struct mahasiswa {
 int main() {
     mahasiswa tyaa;
 
-    // Pengisian data
     tyaa.nim = "c030324111";
     tyaa.nama = "tyaa";
     tyaa.alamat = "jl cemara raya";
     tyaa.ipk = 4.0;
 
-    // Menampilkan data
     cout << tyaa.nim << "\n";
     cout << tyaa.nama << "\n";
     cout << tyaa.alamat << "\n";
     cout << tyaa.ipk << "\n";
 
-    // Input nama dan salam
     cout << "\nMasukkan nama baru: ";
     cin >> tyaa.nama;
 
-    system("cls"); // hanya akan berfungsi di Windows
+    system("cls"); 
     cout << "Halo " << tyaa.nama << endl;
 
     return 0;
